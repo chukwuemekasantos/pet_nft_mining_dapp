0b6f5e02efde63a41401cefd3180893ae23ba9e0
chukwuemekasantos <projectx@ProjectXs-MacBook-Pro.local>
2022-09-14 04:14:18 -0700

    animal ntf dapp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './header/Nabar';
+import NTFimages from './component/NTFimages';
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [connect,setConnected] = useState();
+  const [signer,setSigner] = useState();
+  
+  console.log(signer)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar setSigner={setSigner} isConnected={setConnected}/>
+      <NTFimages isConnected={connect}  signer={signer}/>
     </div>
   );
 }
