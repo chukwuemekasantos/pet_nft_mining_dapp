fee5c70461f3cb2a0d53b4d17fc55a7b70a1eaa8
chukwuemekasantos <projectx@ProjectXs-MacBook-Pro.local>
2022-09-14 04:18:41 -0700

    change dapp name

--- src/header/Nabar.js
+++ src/header/Nabar.js
@@ -43,7 +43,7 @@ function Navbar(props) {
         
         <nav className="navbar navbar-expand-lg navbar-light bg-light p-2">
             <div className="container-fluid">
-                <a className="navbar-brand" href="#">Animal NFT</a>
+                <a className="navbar-brand" href="#">Pet NFT</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
